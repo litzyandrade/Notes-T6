db2560a842afd613cb7053e1c2d85bd084e2bc51
Edgar Monroy <edgar.monroy@capgemini.com>
2021-08-16 17:32:43 -0500

    Agregando el que se muestren los usuarios y sus notas en el dashboard y las funciones para eliminar una nota en especifico

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,28 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../interfaces/user/user.module';
 import { UserService } from '../servicio/user.service';
+import { Notas } from '../notas/notas.module';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-users : User[]=[];
-  constructor(private userService: UserService) { }
+  users : User[]=[];
+  notas : Notas[] = [];
+  mensaje: any = "";
 
-  ngOnInit(): void {
-    this.getUsers();
+    constructor(private userService: UserService) { }
+
+    ngOnInit(): void {
+      this.getUsers();
+      this.getNotas();
+    }
+
+    userEncontrado: User = {
+      nombre: "test",
+      apellido: "test",
+      telefono: "123456",
+      email: "mail2@mail.com",
+      password: "123456789",
+      notes: [{
+        titulo: "notsss",
+        descripcion: "asdasdaasd",
+        tipo: "Urgente",
+        fechaA: new Date(),
+        fechaT: new Date()
+      },
+    ]
+    }
+
+    notaActual: Notas = {
+      titulo: "",
+      descripcion: "",
+      tipo: "",
+      fechaA: new Date(),
+      fechaT: new Date()
+    }
+
+
+
+  mostrarNotas(email : String){
+    this.notas = []
+    this.userEncontrado = this.users.filter(user => user.email == email)[0]
+    this.userEncontrado.notes.forEach(nota => {
+      this.notas.push(nota) } ) 
+
   }
-mostrarNotas(){
-  console.log("notaaaa")
-}
-  async getUsers(){
+
+  async eliminarNotas(titulo : String){
     try{
-      
-      this.users = await this.userService.getUsers();
-
-    }catch(err){
-        console.log(err);
-    }    
+      this.notaActual = this.notas.filter(nota => nota.titulo == titulo)[0]
+      this.mensaje = await this.userService.eliminarNota(this.notaActual)
+      console.log(this.mensaje)
+    }
+    catch(err){
+      console.log(err);
+    }
   }
+
+    async getUsers(){
+      try{
+
+        this.users = await this.userService.getUsers();        
+
+      }catch(err){
+          console.log(err);
+      }    
+    }
+
+    
+    async getNotas(){
+      try{
+        
+        //this.notas = await this.userService.getNota();
+
+      }catch(err){
+          console.log(err);
+      }    
+    }
 }
